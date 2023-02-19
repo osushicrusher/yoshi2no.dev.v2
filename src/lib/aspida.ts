816d24764e4eaa776f6d72d5d65e273115960bc2
Hikaru Yoshino <osushicrusher@gmail.com>
2023-02-19 22:00:17 +0900

    feat: aspidaクライアントの作成

---
+++ src/lib/aspida.ts
@@ -0,0 +1,10 @@
+import aspida, { FetchConfig } from "@aspida/fetch";
+import api from "@api/$api";
+
+const fetchConfig: FetchConfig = {
+  credentials: "include",
+  baseURL: "api",
+  throwHttpErrors: true, // throw an error on 4xx/5xx, default is false
+};
+
+export const client = api(aspida(fetch, fetchConfig));
