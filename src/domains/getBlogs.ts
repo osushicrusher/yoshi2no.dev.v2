a54dc02d99eb845bd6f50377c5acbd9a5306da89
Hikaru Yoshino <osushicrusher@gmail.com>
2023-02-21 22:28:25 +0900

    fix: getBlogsの修正

--- src/domains/getBlogs.ts
+++ src/domains/getBlogs.ts
@@ -1,12 +1,16 @@
 import { HTTPError } from "@aspida/fetch";
-import { BlogSchema } from "@/models/Blog";
+import { BlogsSchema } from "@/models/Blog";
 import { ErrorResponseSchema } from "@/models/ErrorResponse";
 import { client } from "@/lib/aspida";
 
 export const getBlogs = async () => {
   try {
-    const res = await client.blogs.$get();
-    return BlogSchema.parse(res);
+    const res = await client.microcms.blogs.$get({
+      headers: {
+        "X-MICROCMS-API-KEY": import.meta.env.MICROCMS_API_KEY,
+      },
+    });
+    return BlogsSchema.parse(res.contents);
   } catch (e) {
     if (e instanceof HTTPError) {
       ErrorResponseSchema.parse(await e.response.json());
